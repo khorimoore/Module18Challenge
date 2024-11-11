5332d24433ed3c7e8a602d3f84432556f80f575f
khori moore <khorimoore23@gmail.com>
2024-11-11 14:40:56 -0800

    integrated apollo into main.tsx file

--- Develop/client/src/main.tsx
+++ Develop/client/src/main.tsx
@@ -1,28 +1,40 @@
-import ReactDOM from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import ReactDOM from 'react-dom/client';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App from './App';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
 
-import App from './App.jsx'
-import SearchBooks from './pages/SearchBooks'
-import SavedBooks from './pages/SavedBooks'
+// Initialize Apollo Client
+const client = new ApolloClient({
+  uri: 'http://localhost:3001/graphql', // Update to your GraphQL server URI if different
+  cache: new InMemoryCache(),
+});
 
+// Set up the router with routes for the app
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
-    errorElement: <h1 className='display-2'>Wrong page!</h1>,
+    errorElement: <h1 className="display-2">Wrong page!</h1>,
     children: [
       {
         index: true,
-        element: <SearchBooks />
-      }, {
-        path: '/saved',
-        element: <SavedBooks />
-      }
-    ]
-  }
-])
+        element: <SearchBooks />,
+      },
+      {
+        path: 'saved',
+        element: <SavedBooks />,
+      },
+    ],
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={router} />
-)
+// Render the app with ApolloProvider and RouterProvider
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <ApolloProvider client={client}>
+    <RouterProvider router={router} />
+  </ApolloProvider>
+);
